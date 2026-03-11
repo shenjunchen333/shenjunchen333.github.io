efda1e695e4579a6dacd8289e713d5fe50278701
Junchen Shen <shenjunchen0923@outlook.com>
2026-03-11 17:13:45 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("Welcome to Junchen Shen's website!");
+
+const button = document.getElementById("themeToggle");
+
+button.onclick = function(){
+
+document.body.classList.toggle("dark-mode");
+
+};
